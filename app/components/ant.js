c7ae2b9335a9156e0a26cab116cac3fa714535e5
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-20 12:01:17 -0400

    first commit

---
+++ app/components/ant.js
@@ -0,0 +1,13 @@
+'use client'
+import { DatePicker } from 'antd'
+import React from 'react'
+
+const Ant = () => {
+  return (
+    <div>
+      <DatePicker />
+    </div>
+  )
+}
+
+export default Ant
