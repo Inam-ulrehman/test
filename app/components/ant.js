fb7082ac1776732425d17176c1d83581bc278093
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-20 12:06:50 -0400

    fix

--- app/components/ant.js
+++ app/components/ant.js
@@ -1,5 +1,5 @@
 'use client'
-import { Button, DatePicker } from 'antd'
+import { Button, DatePicker, Divider, Space } from 'antd'
 import React from 'react'
 
 const Ant = () => {
@@ -7,6 +7,14 @@ const Ant = () => {
     <div>
       <DatePicker />
       <Button type='primary'>Primary Button</Button>
+      <Divider />
+      <Space wrap>
+        <Button type='primary'>Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type='dashed'>Dashed Button</Button>
+        <Button type='text'>Text Button</Button>
+        <Button type='link'>Link Button</Button>
+      </Space>
     </div>
   )
 }
