343187bf34076e1a2f3b31e2f748baab5a3de1ae
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-20 12:34:41 -0400

    fix

--- app/providers.js
+++ app/providers.js
@@ -5,7 +5,7 @@ import customTheme from './styles/theme'
 export function Providers({ children }) {
   return (
     <>
-      <ConfigProvider theme={customTheme()}>{children}</ConfigProvider>
+      <ConfigProvider>{children}</ConfigProvider>
     </>
   )
 }
