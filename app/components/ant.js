6fcdac4a548eb012764c0aac5510abcc80ae6a4c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-21 12:03:36 -0400

    fix

--- app/components/ant.js
+++ app/components/ant.js
@@ -4,21 +4,7 @@ import { Button } from 'antd'
 import styled from 'styled-components'
 
 const Ant = () => {
-  return (
-    <Wrapper>
-      <h1>heading 1</h1>
-      <h2>heading 2</h2>
-      <h3>heading 3</h3>
-      <h4>heading 4</h4>
-      <h5>heading 5</h5>
-      <p>
-        how are you Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-        molestiae non excepturi similique, repudiandae ab voluptas sed! Minus,
-        ullam. Corrupti, sit. Assumenda nisi impedit perferendis.
-      </p>
-      <Button>Change theme</Button>
-    </Wrapper>
-  )
+  return <Wrapper></Wrapper>
 }
 const Wrapper = styled.div`
   .ant-btn {
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Ant from './components/ant'
+import Theme from './components/theme'
 
 export default function Home() {
   return (
     <main>
       <h1>hello</h1>
       <Ant />
+      <Theme />
     </main>
   )
 }
