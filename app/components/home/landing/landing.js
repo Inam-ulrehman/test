cfec3df58423474a6c7c1cd906d0e6da92943b81
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-23 11:48:45 -0400

    fix

---
+++ app/components/home/landing/landing.js
@@ -0,0 +1,15 @@
+'use client'
+import styled from 'styled-components'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <h1>Home Page</h1>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - 45px);
+`
+export default Landing
--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Landing from './components/home/landing/landing'
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <Landing />
     </main>
   )
 }
