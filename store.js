f82b42b14bc82c764c87a62270188ad116c6e415
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-30 13:12:35 -0400

    adding contacts

--- store.js
+++ store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import samplesSlice from './features/samples/samplesSlice'
 import usersSlice from './features/users/usersSlice'
+import contactsSlice from './features/contacts/contactsSlice'
 
 const store = configureStore({
   reducer: {
     samples: samplesSlice,
     users: usersSlice,
+    contacts: contactsSlice,
   },
 })
 
