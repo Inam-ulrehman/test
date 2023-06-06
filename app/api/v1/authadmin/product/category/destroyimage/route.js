8427a91989672ca6872f3decd10d43c36ce6bc07
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-06 12:11:16 -0400

    api destroy

--- app/api/v1/authadmin/product/category/destroyimage/route.js
+++ app/api/v1/authadmin/product/category/destroyimage/route.js
@@ -3,13 +3,35 @@ import mongooseErrorHandler from '@/lib/errors/mongoose-error-handler'
 import { titleCase } from '@/lib/helper'
 import User from '@/models/User'
 import { StatusCodes } from 'http-status-codes'
+const cloudinary = require('cloudinary').v2
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 export async function POST(request, res) {
-  const pathName = request.nextUrl.pathname
-  const searchParams = request.nextUrl.searchParams
-  const cookies = request.cookies.getAll()
   const body = await request.json()
   const { public_id } = body
 
-  return new Response(JSON.stringify({ public_id }), { status: StatusCodes.OK })
+  try {
+    const result = await cloudinary.uploader.destroy(public_id)
+    return new Response(
+      JSON.stringify({ success: true, msg: 'Image deleted', result }),
+      {
+        status: StatusCodes.OK,
+      }
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        msg: 'Something went wrong',
+        result: error.result,
+      }),
+      {
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+      }
+    )
+  }
 }
