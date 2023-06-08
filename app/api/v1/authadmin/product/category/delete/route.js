b84dc4c3c36f6843072084384e9dfc42c52a0f75
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-08 15:06:34 -0400

    fix

--- app/api/v1/authadmin/product/category/delete/route.js
+++ app/api/v1/authadmin/product/category/delete/route.js
@@ -4,7 +4,12 @@ import mongooseErrorHandler from '@/lib/errors/mongoose-error-handler'
 import Category from '@/models/product/Category'
 import { StatusCodes } from 'http-status-codes'
 import { NextResponse } from 'next/server'
-
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 export async function POST(request, res) {
   const body = await request.json()
   const { _id } = body
@@ -12,13 +17,40 @@ export async function POST(request, res) {
   await dbConnect()
 
   try {
-    const result = await Category.findByIdAndDelete({ _id })
-    if (!result) {
+    const product = await Category.findById({ _id })
+
+    if (!product) {
       return NextResponse.json(
         { success: false, msg: 'No results found' },
         { status: StatusCodes.NOT_FOUND }
       )
     }
+
+    const publicIds = product.images.map((image) => image.response.public_id)
+
+    // delete image from cloudinary
+    if (publicIds.length > 0) {
+      const cloudDestroy = await cloudinary.api.delete_resources(publicIds)
+
+      const hasNotFound = Object.values(cloudDestroy.deleted).includes(
+        'not_found'
+      )
+      // cloudinary returns not_found if image is not found
+      const result = await Category.deleteOne({ _id })
+      if (hasNotFound) {
+        return NextResponse.json(
+          {
+            success: false,
+            msg: 'Cloudinary Not found deleted from database',
+            result,
+          },
+          { status: StatusCodes.OK }
+        )
+      }
+    }
+
+    // delete product from database
+    const result = await Category.deleteOne({ _id })
     return NextResponse.json(
       { success: true, msg: 'Successfully Deleted!', result },
       { status: StatusCodes.OK }
